e1b7fbffd52b6d00cf85b08098402c79ba76f472
Antonio Perez Caballero <antonio.gialnet@gmail.com>
2018-12-03 18:46:41 +0100

    simple rest with spring boot 2.1.1 release

---
+++ simple-rest/src/main/java/com/vivaldispring/simplerest/controllers/CheckVersion.java
@@ -0,0 +1,27 @@
+package com.vivaldispring.simplerest.controllers;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+@CrossOrigin(origins = "*")
+public class CheckVersion {
+    Map<String, Object> response = new HashMap<String, Object>();
+
+    @RequestMapping(value = "/ckversion", method = RequestMethod.GET, produces = {"application/JSON"} )
+    public ResponseEntity<?> checkVersion(){
+
+        response.clear();
+        response.put("version", "1.0");
+        response.put("status", "ok");
+        response.put("data",  "Version 2.0 Release August 2018" );
+        return new ResponseEntity<>(response, HttpStatus.OK);
+    }
+}
